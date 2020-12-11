b3e3ae2f5d324a27c83b159d0d208c5e03b41edc
ALEJANDRO RUSCA <alejandroruscamoreno@gmail.com>
2020-12-11 03:30:47 +0000

    [Fix]: change logic

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""lockboxes"""
+"""Lockboxes"""
 
 
 def canUnlockAll(boxes):
-    """check if all of the lockboxes can be opened"""
-    newlist = []
-    k = len(boxes)
-    for i in boxes:
-        if len(i) == 0 and i is not boxes[k-1]:
-            return False
-        for j in i:
-            newlist.append(j)
-    for index, keys in enumerate(boxes):
-        if index in newlist or index < k-1:
-            return True
-        else:
-            return False
+    """Cheks if a lockbox can be opened with each key"""
+    used_keys = [0]
+    while (1):
+        new_key_added = False
+        for i, keys in enumerate(boxes):
+            if i in used_keys:
+                for key in keys:
+                    if key not in used_keys and (0 <= key < len(boxes)):
+                        new_key_added = True
+                        used_keys.append(key)
+        if new_key_added is False:
+            break
+    if len(used_keys) == len(boxes):
+        return True
+    else:
+        return False
