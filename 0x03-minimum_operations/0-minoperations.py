3a86dca2291618e3d29364a469cce8f4270ce928
dondropo <alejandroruscamoreno@gmail.com>
2021-05-10 13:17:33 -0500

    Added task 0

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+def minOperations(n):
+    """ minimun operations, returns int """
+    if (n <= 1):
+        return 0
+
+    hs = 1
+    processCount = 0
+    copypaste = 0
+
+    while (hs < n):
+        if (n % hs == 0):
+            processCount += 2
+            copypaste = hs
+        else:
+            processCount += 1
+
+        hs = copypaste + hs
+
+    return processCount
